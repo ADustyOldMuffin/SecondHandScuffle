56e93810aa9e2ce6e25a28e1a3c1debc1bf8028d
Daniel Hix <danieljacobhix@gmail.com>
2021-01-02 17:45:23 -0600

    Add player dying

---
+++ Assets/Scripts/Managers/Levels/MainGameManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Constants;
+using UnityEngine;
+
+namespace Managers.Levels
+{
+    public class MainGameManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            LevelManager.OnPlayerDeath += PlayerDied;
+        }
+
+        private void PlayerDied()
+        {
+            StartCoroutine(WaitAndGameOver());
+        }
+
+        private IEnumerator WaitAndGameOver()
+        {
+            yield return new WaitForSeconds(2);
+            LevelManager.Instance.LoadLevel((int)Level.GameOver);
+        }
+    }
+}
