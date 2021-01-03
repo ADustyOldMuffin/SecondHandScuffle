42f26bf759ef208ff19d3e06ed4eec2815c6575f
JarOfWombats <76613679+JarOfWombats@users.noreply.github.com>
2021-01-03 15:01:16 +1000

    basic enemy prefabs update

--- Assets/Scripts/Enemies/EnemyProjectile.cs
+++ Assets/Scripts/Enemies/EnemyProjectile.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Linq;
+using Managers;
 using UnityEngine;
 
-public class EnemyProjectile : MonoBehaviour
+namespace Weapons.Projectiles
 {
-    // Start is called before the first frame update
-    void Start()
+    public class EnemyProjectile : BaseProjectile
     {
-        
-    }
+        protected override bool HurtsPlayer { get; } = true;
+        protected override int DamageAmount { get; } = 1;
+        protected override float ProjectileLifetime { get; } = 10f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField] private float moveSpeed = 10f;
+        [SerializeField] private Rigidbody2D myRigidbody;
+
+        private float _aliveFor = 0.0f;
+
+        private void FixedUpdate()
+        {
+            var myTransform = transform;
+            var newPosition = myTransform.position + _moveDirection * (moveSpeed * Time.fixedDeltaTime);
+            myRigidbody.MovePosition(newPosition);
+
+            if (_aliveFor >= ProjectileLifetime)
+                Destroy(gameObject);
+
+            _aliveFor += Time.fixedDeltaTime;
+        }
     }
 }
+
