5695fbfda50e16d90006b0be707432f4314bb0be
JarOfWombats <76613679+JarOfWombats@users.noreply.github.com>
2021-01-03 11:25:49 +1000

    EnemySpawner
    Basic EnemySpawner is up and running

---
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject[] Enemies;
+    [SerializeField] float spawnTimerMin = 2f;
+    [SerializeField] float spawnTimerMax = 10f;
+
+    bool spawn = true;
+
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+
+            yield return new WaitForSecondsRealtime(Random.Range(spawnTimerMin, spawnTimerMax));
+            if (spawn) { SpawnEnemies(); }
+
+        }
+    }
+
+
+    public void StopSpawning()
+    {
+        spawn = false;
+    }
+
+    private void SpawnEnemies()
+    {
+        GameObject enemy = Instantiate(Enemies[Random.Range(0, Enemies.Length)],
+                transform.position,
+                transform.rotation) as GameObject;
+
+        enemy.transform.parent = transform;
+
+    }
+}
--- Assets/Scripts/Managers/Levels/MainGameManager.cs
+++ Assets/Scripts/Managers/Levels/MainGameManager.cs
@@ -8,14 +8,18 @@ namespace Managers.Levels
 {
     public class MainGameManager : MonoBehaviour
     {
+        EnemySpawner[] enemySpawners;
         private void Awake()
         {
             LevelManager.OnPlayerDeath += PlayerDied;
+            enemySpawners = FindObjectsOfType<EnemySpawner>();
         }
 
         private void PlayerDied()
         {
             Debug.Log("About to die for real");
+            StopSpawners();
+            //StartCoroutine(WaitAndGameOver());
             LevelManager.Instance.LoadLevel((int)Level.GameOver);
         }
 
@@ -23,6 +27,14 @@ namespace Managers.Levels
         {
             yield return new WaitForSeconds(2);
             LevelManager.Instance.LoadLevel((int)Level.GameOver);
+        }
+
+        private void StopSpawners()
+        {
+            for(int i = 0; i < enemySpawners.Length; i++)
+            {
+                enemySpawners[i].StopSpawning();
+            }
         }
     }
 }
