da9cea23f0b554f07b468dc7166a7d9d511674aa
Daniel Hix <danieljacobhix@gmail.com>
2021-01-02 23:56:53 -0600

    Finished the flamer spreader

---
+++ Assets/Scripts/Weapons/Projectiles/DamageOverTime.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Enemies;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Weapons.Projectiles
+{
+    public class DamageOverTime : MonoBehaviour
+    {
+        public int DamagePerTick { get; set; } = 1;
+        public float TickTime { get; set; } = 1f;
+        public int Ticks { get; set; } = 3;
+
+        private int _ticksPassed = 0;
+        private float _currentTime = 0.0f;
+        private EnemyHealth _enemy;
+
+        private void Awake()
+        {
+            // 
+            if(!TryGetComponent<EnemyHealth>(out _enemy))
+                Destroy(this);
+        }
+
+        private void FixedUpdate()
+        {
+            _currentTime += Time.fixedDeltaTime;
+
+            if (_currentTime >= TickTime)
+            {
+                _currentTime = 0.0f;
+                _ticksPassed += 1;
+                _enemy.Damage(DamagePerTick, 0.5f, new Vector2(Random.Range(-1, 2), Random.Range(-1, 2)));
+            }
+            
+            if(_ticksPassed >= Ticks)
+                Destroy(this);
+        }
+
+        public void ResetTicks()
+        {
+            _ticksPassed = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/Projectiles/FireProjectile.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Animancer;
+using UnityEngine;
+
+namespace Weapons.Projectiles
+{
+    public class FireProjectile : BaseProjectile
+    {
+        protected override int DamageAmount { get; } = 1;
+        
+        [SerializeField] private Component dotComponent;
+        [SerializeField] private int dotTicks = 3;
+        [SerializeField] private int tickDamage = 2;
+        [SerializeField] private float tickTime = 1f;
+        [SerializeField] private AnimancerComponent animancer;
+        [SerializeField] private AnimationClip fireClip;
+
+        private float _timer = 0.0f;
+
+        private void Awake()
+        {
+            _timer = fireClip.length;
+            animancer.Play(fireClip);
+        }
+
+        private void FixedUpdate()
+        {
+            _timer -= Time.fixedDeltaTime;
+            
+            if(_timer <= 0f)
+                Destroy(gameObject);
+        }
+
+        protected override void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.CompareTag("Enemy"))
+                return;
+
+            if (other.gameObject.TryGetComponent(out DamageOverTime existingDot))
+            {
+                existingDot.ResetTicks();
+                return;
+            }
+            
+            var dot = other.gameObject.AddComponent<DamageOverTime>();
+            dot.Ticks = dotTicks;
+            dot.TickTime = tickTime;
+            dot.DamagePerTick = tickDamage;
+        }
+    }
+}
