e14ea33e3d292800e8b102803f278ee72f52ec84
Daniel Hix <danieljacobhix@gmail.com>
2021-01-02 20:18:00 -0600

    Added bat, also removed some debug

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -20,7 +20,7 @@ namespace Player
 
         public void DamagePlayer(int amount)
         {
-            Debug.Log(GetPlayerHealth());
+            //Debug.Log(GetPlayerHealth());
             if (_currentHealth - amount <= 0)
             {
                 LevelManager.PlayerDied();
---
+++ Assets/Scripts/Weapons/Projectiles/BaseballBalProjectile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Weapons.Projectiles
+{
+    public class BaseballBalProjectile : BaseProjectile
+    {
+        protected override bool HurtsPlayer { get; } = false;
+        protected override int DamageAmount { get; } = 1;
+        protected override float ProjectileLifetime { get; } = .25f;
+
+        private float _aliveFor = 0.0f;
+        
+        protected override void OnCollisionEnter2D(Collision2D other)
+        {
+            return;
+        }
+
+        private void FixedUpdate()
+        {
+            if(_aliveFor >= ProjectileLifetime)
+                Destroy(gameObject);
+
+            _aliveFor += Time.fixedDeltaTime;
+        }
+    }
+}
