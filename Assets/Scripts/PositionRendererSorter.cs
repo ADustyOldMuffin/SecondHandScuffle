f1adaa9bd0a083a0ea38685af6caf145c154ebb6
JarOfWombats <76613679+JarOfWombats@users.noreply.github.com>
2021-01-13 19:30:48 +1000

    added bg collider

---
+++ Assets/Scripts/PositionRendererSorter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionRendererSorter : MonoBehaviour
+{
+    [SerializeField] private int sortingOrderBase = 5000;
+    private Renderer myRenderer;
+    [SerializeField] private int offset = 0;
+    [SerializeField] private bool runOnlyOnce = false;
+
+    private float timer;
+    private float timerMax = .1f;
+   
+
+    private void Awake()
+    {
+        myRenderer = gameObject.GetComponent<Renderer>();
+    }
+
+    //update about trasform has moved
+    private void LateUpdate()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            timer = timerMax;
+
+            myRenderer.sortingOrder = (int)(sortingOrderBase - transform.position.y - offset);
+            if(runOnlyOnce)
+            {
+                Destroy(this);
+            }
+        }
+
+    }
+}
